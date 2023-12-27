40529adbbd939598096b4453f17e93be0c0e7306
Niladri <niladrisengit12@gmail.com>
2023-12-27 22:40:52 +0530

    day-10

--- client/src/components/shared/Actions.jsx
+++ client/src/components/shared/Actions.jsx
@@ -1,43 +1,73 @@
+import axios from "axios";
 import { useState } from "react";
 import { BiRepost } from "react-icons/bi";
 import { FaHeart, FaRegComment, FaRegHeart } from "react-icons/fa";
 import { FaShareFromSquare } from "react-icons/fa6";
+import { useThreadContext } from "../../store/ThreadContext";
 
-const Actions = () => {
-    const [liked, setLiked] = useState(false);
+const Actions = ({postId}) => {
+
+
+  const {liked,setLiked} = useThreadContext();
+
+    const likeUnlikePost = async () => {
+        // setUpdating(true)
+        try {
+          const res = await axios.put(`/api/posts/likeOrUnlikePost/${postId}`);
+          if (Array.isArray(liked)) {
+            if(liked?.includes(postId)){
+              const filtered = liked.filter((id) => id !== postId);
+              setLiked(filtered);
+            } else
+                setLiked([...liked, postId])
+          } else {
+            setLiked([postId]);
+          }
+          // setUpdating(false)
+          console.log(res.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
   return (
     <div className="flex items-center gap-1">
-            {!liked ? (
-              <FaRegHeart
-                onClick={() => setLiked(!liked)}
-                className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
-                size={40}
-              />
-            ) : (
-              <FaHeart
-                size={40}
-                onClick={() => setLiked(!liked)}
-                color="red"
-                className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
-              />
-            )}
+      { (liked && !liked?.includes(postId)) ? (
+        <div onClick={likeUnlikePost}>
+          <FaRegHeart
+            onClick={() => setLiked(!liked)}
+            className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
+            size={40}
+          />
+        </div>
+      ) : (
+        <div onClick={likeUnlikePost}>
 
-            <FaRegComment
-              size={40}
-              color="#fff"
-              className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
-            />
-            <BiRepost
-              size={46}
-              color="#fff"
-              className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
-            />
-            <FaShareFromSquare
-              size={40}
-              color="#fff"
-              className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
-            />
+        <FaHeart
+          size={40}
+          onClick={() => setLiked(!liked)}
+          color="red"
+          className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
+          />
           </div>
-  )
-}
-export default Actions
+      )}
+
+      <FaRegComment
+        size={40}
+        color="#fff"
+        className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
+      />
+      <BiRepost
+        size={46}
+        color="#fff"
+        className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
+      />
+      <FaShareFromSquare
+        size={40}
+        color="#fff"
+        className="cursor-pointer hover:bg-gray-800 p-2 rounded-full duration-300 ease-in-out transition-all"
+      />
+    </div>
+  );
+};
+export default Actions;
